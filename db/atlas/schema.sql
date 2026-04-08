b987595adac1f0920d8ff35efdbbdaacee07c66a
abd0-Omar <abdelrahmanorigin@gmail.com>
2026-04-08 11:19:38 +0200

    add todo tags field to db using atlas

---
+++ db/atlas/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS todos (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    tags TEXT[],
+    is_completed BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
